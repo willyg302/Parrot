7c1b0e1891255d615703434da832272eaf06e66a
William Gaul <willyg302@gmail.com>
2013-11-13 15:45:45 -1000

    Logging, threaded ing, threaded user input, and daemon functionality

--- parrot.py
+++ parrot.py
@@ -1,9 +1,38 @@
-import sys, argparse, json
+import sys, argparse, json, logging, threading, Queue
 
 import facebook
 from twython import Twython
 
 tokens = None
+
+APP_NAME = 'Parrot'
+VERSION = '0.1'
+
+def user_input(q, lock):
+	while True:
+		with lock:
+			cmd = raw_input()
+		q.put(cmd)
+		if cmd.lower() == 'exit':
+			break
+
+def handle_input(cmd, lock):
+	print 'Received command: ' + cmd
+
+def init_log(debug, flush):
+	# Set up our basic file logger
+	logging.basicConfig(
+		level=(logging.DEBUG if debug else logging.INFO),
+		filename='{}-log.log'.format(APP_NAME),
+		filemode=('w' if flush else 'a'),
+		format='%(asctime)s %(name)-12s %(levelname)-10s %(message)s',
+		datefmt='%m-%d %H:%M')
+
+	# Define an equivalent console logger
+	console = logging.StreamHandler()
+	console.setLevel(logging.INFO)
+	console.setFormatter(logging.Formatter('%(name)-12s: %(levelname)-10s %(message)s'))
+	logging.getLogger('').addHandler(console)
 
 # Returns True if tokens successfully loaded
 def load_tokens(mode):
@@ -13,16 +42,48 @@ def load_tokens(mode):
 			tokens = json.load(f)[mode]
 		return True
 	except IOError, ValueError:
-		print 'Error loading authorization tokens!'
+		logging.critical('Could not load authorization tokens!')
 		return False
+
+def exit():
+	logging.info('{} exiting...'.format(APP_NAME))
+	logging.info('-' * 40 + '\n')
+	logging.shutdown()
+	sys.exit(0)
 
 # Parrot is being run directly from CLI
 def main():
+
+	# Parse command line arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-d", "--debug", help="set Parrot to debug mode", action="store_true")
+	parser.add_argument("-d", "--debug", help="set {} to debug mode".format(APP_NAME), action="store_true")
+	parser.add_argument("-f", "--flush", help="flush the log file rather than appending", action="store_true")
 	args = parser.parse_args()
+
+	# Start our log
+	init_log(args.debug, args.flush)
+	logging.info('-' * 40)
+	logging.info('{} starting up...'.format(APP_NAME))
+
+	# Load our authorization tokens
 	if not load_tokens('debug' if args.debug else 'production'):
 		sys.exit(1)
+
+	logging.info('{} v{} is up and running'.format(APP_NAME, VERSION))
+	logging.info('-' * 40 + '\n')
+
+	# Our main loop, we spawn a user input thread and associated queues/locks
+	input_queue = Queue.Queue()
+	input_lock = threading.Lock()
+	threading.Thread(target=user_input, args=(input_queue, input_lock)).start()
+	while True:
+		cmd = input_queue.get()
+		if cmd.lower() == 'exit':
+			break
+		handle_input(cmd, input_lock)
+
+	# Since we are out of the loop, we are free to exit
+	exit()
 
 	# Example: get Facebook feed of Obama
 	#graph = facebook.GraphAPI(tokens['fb_token'])
