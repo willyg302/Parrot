baeda27eb8264fe140588418f4c2f083f03638ee
William Gaul <willyg302@gmail.com>
2014-05-05 21:22:34 -1000

    Using strap.py

---
+++ setup-tokens.py
@@ -0,0 +1,20 @@
+import os
+
+
+TOKENS_FILE = os.path.join('parrot', 'tokens.py')
+
+
+# Set up a tokens.py file if it does not already exist
+def init_tokens_file():
+	if not os.path.exists(TOKENS_FILE):
+		import base64, uuid
+		with open(TOKENS_FILE, 'w') as f:
+			f.write('COOKIE_SECRET = "{}"\n'.format(base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)))
+			f.write('FB_TOKEN = "OAuth token for Facebook"\n')
+			f.write('TW_APP_KEY = "This and the next 3 are OAuths for Twitter"\n')
+			f.write('TW_APP_SECRET = ""\n')
+			f.write('TW_OAUTH_TOKEN = ""\n')
+			f.write('TW_OAUTH_SECRET = ""\n')
+
+if __name__ == '__main__':
+	init_tokens_file()
