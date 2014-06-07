183557b63743f7968555b70f76a68f466f79155b
William Gaul <willyg302@gmail.com>
2014-06-07 00:04:48 -0700

    New strapme format

--- strapme.py
+++ strapme.py
@@ -1,5 +1,7 @@
 import os
 
+project = 'Parrot'
+
 TOKENS_FILE = os.path.join('parrot', 'tokens.py')
 
 # Set up a tokens.py file if it does not already exist
@@ -14,27 +16,21 @@ def init_tokens_file():
 			f.write('TW_OAUTH_TOKEN = ""\n')
 			f.write('TW_OAUTH_SECRET = ""\n')
 
-config = {
-	'project': 'Parrot',
-	'tasks': {
-		'install': {
-			'name': 'Install Parrot',
-			'virtualenv': 'tornado',
-			'run': [
-				'easy_install greenlet',
-				'pip install motor',
-				'pip install pattern',
-				init_tokens_file
-			],
-			'freeze': 'requirements.txt'
-		},
-		'server': {
-			'name': 'Start Parrot server',
-			'virtualenv': 'tornado',
-			'run': ['python parrot']
-		},
-		'default': {
-			'run': ['server']
-		}
-	}
-}
+def install():
+	'''Install Parrot'''
+	with strap.virtualenv('tornado'):
+		strap.run([
+			'easy_install greenlet',
+			'pip install motor',
+			'pip install pattern',
+			init_tokens_file
+		])
+		strap.freeze('requirements.txt')
+
+def server():
+	'''Start Parrot server'''
+	with strap.virtualenv('tornado'):
+		strap.run('python parrot')
+
+def default():
+	strap.run(server)
